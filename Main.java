673fd4908c8c69410bffe6cad4b5fbb723718306
Kavya K Rajendran <52087473+krithvi@users.noreply.github.com>
2019-09-25 11:05:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,34 +6,10 @@ public class Main
     public static void main(String args[])
     {
 
-        //Pattern p = new Pattern();
-        //Pattern p1 = new Pattern();
-        //Break b = new Break(p);
-        //p.wall1();
-        //p.wall2();
-        //Break b = new Break();
-        //Session s1 = new Session();
-        //Break b = new Break();
-       //Session s3 = new Session();
-       //Session s4 = new Session();
-
-
-        ///s1.start();
-        //b.start();
-       // s3.start();
-       // s4.start();
-
-        //try {
-            //s1.thread.join();
-           // b.thread.join();
-            //s2.thread.join();
-            // s3.thread.join();
-            // s4.thread.join();
-        //}
-       // catch(InterruptedException e)
-        //{
-          //  System.out.println(e);
-       // }
-
+       
+        Session s1 = new Session();
+        Break b = new Break();
+       Session s2 = new Session();
+       
     }
 }
